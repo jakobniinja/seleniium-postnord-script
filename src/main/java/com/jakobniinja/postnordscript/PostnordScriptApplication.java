0f6b4fcc7970de277d7030ab35754e8c3a51aa79
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-08-07 13:55:29 +0200

    suggestion: suppress slf4j warning about multi bindning

--- src/main/java/com/jakobniinja/postnordscript/PostnordScriptApplication.java
+++ src/main/java/com/jakobniinja/postnordscript/PostnordScriptApplication.java
@@ -10,22 +10,35 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 @SpringBootApplication
 public class PostnordScriptApplication {
     public static void main(String[] args) throws InterruptedException, IOException {
-        CharSequence charSequence = new StringBuffer("14730");
+
+//        <------- Static values --------->
+
+        CharSequence postkod = new StringBuffer("12245");
         final String seleniumUrl = "https://www.postnord.se/vara-verktyg/sok-utdelningsdag";
+
 //        <------- Config --------->
 
         System.setProperty("webdriver.chrome.driver", "/home/niinja/selenium-drivers/chromedriver");
+
         ChromeOptions handlingSSL = new ChromeOptions();
         handlingSSL.setAcceptInsecureCerts(true);
         WebDriver driver = new ChromeDriver(handlingSSL);
 
+
+
+//        System.setProperty("webdriver.chrome.silentOutput", "true");
+//        java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
+//        java.util.logging.Logger.getLogger("org.apache.catalina.core").setLevel(Level.OFF);
+
 //        <------- Config --------->
 
 //        < -------     let the browser load the webelements  ------->
+
         driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get(seleniumUrl);
@@ -33,10 +46,10 @@ public class PostnordScriptApplication {
 
 
         driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
-        driver.findElement(By.xpath("//*[@id=\"block-DeliveryDaySearchBlockProxy-41857\"]/div/pn-zipcode-search/pn-search-field/div/input")).sendKeys(charSequence);
+        driver.findElement(By.xpath("//*[@id=\"block-DeliveryDaySearchBlockProxy-41857\"]/div/pn-zipcode-search/pn-search-field/div/input")).sendKeys(postkod);
         driver.findElement(By.xpath("//*[@id=\"block-DeliveryDaySearchBlockProxy-41857\"]/div/pn-zipcode-search/pn-search-field/div/input")).sendKeys(Keys.ENTER);
 
-        System.out.println("postkod is: " + charSequence);
+        System.out.println("postkod is: " + postkod);
 
 
 //        <------- Snapshot --------->
@@ -49,7 +62,7 @@ public class PostnordScriptApplication {
 
         Thread.sleep(600);
         File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenshotFile, new File(".//screenshoot/" + newDate + ".png"));
+        FileUtils.copyFile(screenshotFile, new File(".//snapshot/" + newDate + ".png"));
 
 
 //        <------- Snapshot --------->
